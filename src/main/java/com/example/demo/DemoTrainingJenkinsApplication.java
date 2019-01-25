36a6458f2e1460f30b261d20c8f1484f40d4c4fe
Your Name <you@example.com>
2019-01-25 12:25:26 +0530

    tet

--- src/main/java/com/example/demo/DemoTrainingJenkinsApplication.java
+++ src/main/java/com/example/demo/DemoTrainingJenkinsApplication.java
@@ -8,6 +8,7 @@ public class DemoTrainingJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoTrainingJenkinsApplication.class, args);
+		//test jenkins...
 	}
 
 }
